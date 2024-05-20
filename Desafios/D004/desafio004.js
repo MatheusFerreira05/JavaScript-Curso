0869044c25a5d898f367ebf2d3f4fbb95668bc5f
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2024-05-20 16:34:47 -0300

    Desafio 004 - Curso

---
+++ Desafios/D004/desafio004.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt('Digite um valor para saber seu sucessor e seu antecessor: '))
+
+let antecessor = numero - 1
+let sucessor = numero + 1
+
+document.write(`O sucessor do número ${numero} é ${sucessor} e o antecessor é ${antecessor}`)
